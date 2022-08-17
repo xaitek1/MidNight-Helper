1d528112a763f29f0a04cce37346ddc84f4a4165
Sergetec <96439803+Sergetec@users.noreply.github.com>
2022-08-17 23:45:06 +0300

    Delete Commands directory

--- Commands/tag.js
+++
@@ -1,9 +0,0 @@
-const { Client, CommandInteraction } = require('discord.js')
-
-module.exports = {
-    name: 'tag',
-    description: 'shows the tags',
-    execute(client, interaction){
-        interaction.followUp('ﾉᴹⁱᵈᴺ, ᵐⁿ, ᴹⁱᵈᴺ');
-    }
-}
